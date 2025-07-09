6c81dde167425200b504f9ac49afdb72fb54cb50
Abdulloh Mun'am <49928894+AbdullohMunam@users.noreply.github.com>
2025-07-09 06:32:36 +0000

    mengedit agar bekerja dengan 1 app

---
+++ portfolio/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog_list, name='blog'),
+]
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
+
+def projects(request):
+    return render(request, 'blog/project.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+def blog_list(request):
+    return render(request, 'blog/blog.html')
